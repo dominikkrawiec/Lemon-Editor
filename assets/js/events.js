df676c4abdc5eddc67306587507269ac42a75ffb
dwadolary <dominik_krawiec@o2.pl>
2017-10-08 18:32:59 +0200

    Setting caret position

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,3 +1,4 @@
+
 var content = document.querySelector('.content'),
     title = document.querySelector('header'),
     lead = document.querySelector('.lead'),
@@ -5,16 +6,28 @@ var content = document.querySelector('.content'),
 
 // save content to localStorage when element is changed
 content.onkeyup= function(){
+    // saving current caret position
+    var caret = getCaretCharacterOffsetWithin(content);
+    localStorage.setItem('cursor', caret);
     localStorage.setItem('content', content.innerHTML);
+    localStorage.setItem('currentElement', 'content');
     };
 
 title.onkeyup= function(){
+    var caret = getCaretCharacterOffsetWithin(title);
+    localStorage.setItem('cursor', caret);
     localStorage.setItem('title', title.innerHTML);
+    localStorage.setItem('currentElement', 'title');
     };
 
 lead.onkeyup= function(){
+    var caret = getCaretCharacterOffsetWithin(lead);
+    localStorage.setItem('cursor', caret);
     localStorage.setItem('lead', lead.innerHTML);
+    localStorage.setItem('currentElement', 'lead');
+
     };
+
 
 
 shareBtn.addEventListener('click', function(){
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,10 +1,17 @@
+
+
 // Make connection to the server
 var socket = io.connect('http://localhost:3000');
 
-var editor = document.querySelector('.content');
+var editor = document.querySelector('.content'),
+    title = document.querySelector('header'),
+    lead = document.querySelector('.lead');
+
 document.onkeyup = function(){
   socket.emit('editor', {
-    content: editor.innerHTML
+    content: editor.innerHTML,
+    lead: lead.innerHTML,
+    title: title.innerHTML
   } );
 
   console.log('tralalal');
@@ -12,4 +19,13 @@ document.onkeyup = function(){
 
 socket.on('editor', function(data){
   editor.innerHTML = data.content;
+  lead.innerHTML = data.lead;
+  title.innerHTML = data.title;
+
+  let currentElement = document.querySelector('.' + localStorage.getItem('currentElement'));
+
+  // setting current caret position
+  createRange(editor, 5, false);
+  setCurrentCursorPosition(localStorage.getItem('cursor'), currentElement);
+
 })
